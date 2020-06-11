acf6d061dc6de3f60acc13dace7cc400662449d8
UsuiDaisuke <kd1259138@st.kobedenshi.ac.jp>
2020-06-11 10:11:09 +0900

    [GameObject実装]

--- app/src/main/java/com/example/myapplication/App.java
+++ app/src/main/java/com/example/myapplication/App.java
@@ -1,13 +1,6 @@
 package com.example.myapplication;
 
 
-import android.content.SharedPreferences;
-import android.graphics.Matrix;
-import android.util.Log;
-import android.view.SoundEffectConstants;
-import android.hardware.Sensor;
-
-
 /*
 * App
 * リソース管理などのシステム面全般
@@ -26,9 +19,9 @@ public class App
     // 諸々の初期化は終わっているので、ここでロードをかけてもOK
     public void Start()
     {
-        soundManager.PlayBGM("bgm.mp3");
-        se1 = soundManager.LoadSE("se1.mp3");
-        se2 = soundManager.LoadSE("se2.mp3");
+        soundManager.PlayBGM("Sounds/bgm.mp3");
+        se1 = soundManager.LoadSE("Sounds/se1.mp3");
+        se2 = soundManager.LoadSE("Sounds/se2.mp3");
     }
 
 
@@ -45,27 +38,6 @@ public class App
         if(p==null){ return true; }
 
 
-
-        /*
-        if(p.OnDown()) // 押された瞬間
-        {
-            vp.x = p.GetDownPos().x;
-            vp.y = p.GetDownPos().y;
-            vm.Clear();
-            soundManager.PlaySE(se1);
-        }
-
-        if(p.OnFlick()) // 離された＆それがフリックと判定された
-        {
-            vm = p.GetDtoU();
-        }
-
-        if(p.OnUp())
-        {
-            soundManager.PlaySE(se2);
-            Log.d("touch", "OnUP!!!!!");
-        }
-        */
 
         // 必ずゲーム更新の後に行う事
         touchManager.Update();
@@ -85,9 +57,9 @@ public class App
     // ここが安定して動いているとは思わないでください。
     public void Draw()
     {
-        imageManage.Draw("ball.png", vp.x, vp.y);
-        imageManage.Draw("ball.png", 100, 100, 45);
-        imageManage.Draw("ball.png", 200, 100, 2,2,0);
+        imageManage.Draw("Sprites/ball.png", vp.x, vp.y);
+        imageManage.Draw("Sprites/ball.png", 100, 100, 45);
+        imageManage.Draw("Sprites/ball.png", 200, 100, 2,2,0);
     }
 
     // ホームボタンなどを押して裏側へ回った時
@@ -99,7 +71,7 @@ public class App
     // 再度アクティブになった時
     public void Resume()
     {
-        soundManager.PlayBGM("bgm.mp3");
+        soundManager.PlayBGM("Sounds/bgm.mp3");
     }
 
     // <--------------------------------------------------ゲームの実装
---
+++ app/src/main/java/com/example/myapplication/GameObject.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import android.graphics.Rect;
+
+public class GameObject {
+    //====================変数====================
+    enum TYPE_ID{NONE, GROUND, PLAYER, ENEMY};
+    protected  TYPE_ID type = TYPE_ID.NONE;
+    protected boolean m_alive = true;
+
+    protected int m_posX;
+    protected int m_posY;
+
+    protected int m_moveX;
+    protected int m_moveY;
+
+    protected Rect m_hitArea = new Rect();
+    protected String m_imageName = "Sprites/ball.png";
+
+    //====================値渡し関数====================
+    public void SetPos(int x, int y)
+    {
+        m_posX = x;
+        m_posY = y;
+    }
+
+    public void SetMove(int x, int y)
+    {
+        m_moveX = x;
+        m_moveY = y;
+    }
+
+    public void SetImageName(String img)
+    {
+        m_imageName = img;
+    }
+
+    public Rect GetHitArea()
+    {
+        return m_hitArea;
+    }
+    //矩形同士当たり判定
+    public boolean HitCheck(Rect hit)
+    {
+        return m_hitArea.contains(hit);
+    }
+    //矩形・点の当たり判定
+    public boolean HitCheck(int x, int y)
+    {
+        return m_hitArea.contains(x, y);
+    }
+
+    //====================メイン処理関数====================
+
+    public void Init(){}
+    public void Update(){}
+
+    public void Draw()
+    {
+        App.Get().ImageMgr().Draw(m_imageName, m_posX, m_posY);
+    }
+}
